89bbd81b5157d62b865fec47b2ca55c71e43c3ea
perzit0 <percy19marceliano@gmail.com>
2026-07-02 09:41:44 -0500

    feat

--- routes/datos_routes.py
+++ routes/datos_routes.py
@@ -28,8 +28,8 @@ def recibir_datos():
     pm = data.get("pm")
 
 # Plaza Dos de Mayo, Lima - coordenadas por defecto mientras el GPS obtiene fix
-    lat = data.get("lat") or -12.055278
-    lng = data.get("lng") or -77.043611  
+    lat = data.get("lat") or -12.055100
+    lng = data.get("lng") or -77.047900 
     ahora = datetime.utcnow()
 
     # 1) Siempre actualizamos la "última lectura" (mapa en vivo)
